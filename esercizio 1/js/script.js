77385725d491699c065f36586867fc5bd24f6ea1
Massimiliano-Novello <massinov5@gmail.com>
2023-03-24 17:48:48 +0100

    esercizio 1

---
+++ esercizio 1/js/script.js
@@ -0,0 +1,52 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+//Creo un arrey di oggetti
+const biciclette = [
+    {
+        nome: "Bianchi",
+        peso: 15
+    },
+    {
+        nome: "Atala",
+        peso: 10
+    },
+    {
+        nome: "Olmo",
+        peso: 8
+    },
+    {
+        nome: "Casati",
+        peso: 11
+    },
+    {
+        nome: "Colnago",
+        peso: 7
+    },
+    {
+        nome: "Bottecchia",
+        peso: 9
+    },
+    {
+        nome: "Cinelli",
+        peso: 12 
+    }
+];
+
+const pesoBicicletta = biciclette.reduce((item, bici) => item.peso < bici.peso ? item : bici);
+console.log(pesoBicicletta);
+
+creazioneFrase();
+
+function creazioneFrase () {
+    const container = document.querySelector(".container");
+    console.log(container);
+    const result = `<h2>La bicicletta ${pesoBicicletta.nome} pesa solo ${pesoBicicletta.peso}kg ed è la più leggera </h2>`;
+    container.innerHTML = result;
+}
+
+
+
+
